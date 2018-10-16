ec91858724cba879e3a38a6b6d6649abb5c3fd07
Mikhail Svetov <svmk-tmsk@yandex.ru>
2018-10-16 19:17:07 +0700

    Improved markdown generator.

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/StaticMarkdownGenerator.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/StaticMarkdownGenerator.java
@@ -19,7 +19,6 @@ package org.openapitools.codegen.languages;
 
 import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.media.ComposedSchema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import org.openapitools.codegen.*;
 import org.openapitools.codegen.utils.ModelUtils;
@@ -42,6 +41,7 @@ public class StaticMarkdownGenerator extends DefaultCodegen implements CodegenCo
         modelTemplateFiles.put("model.mustache", ".md");
         apiTemplateFiles.put("operation.mustache", ".md");
         supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+        cliOptions.add(new CliOption("markdownBasePath", "base path to markdown root"));
     }
 
     @Override
@@ -72,8 +72,29 @@ public class StaticMarkdownGenerator extends DefaultCodegen implements CodegenCo
     @Override
     public CodegenProperty fromProperty(String name, Schema p) {
         CodegenProperty result = super.fromProperty(name, p);
-        if (!result.isPrimitiveType && !result.isNotContainer) {
-            result.vendorExtensions.put("resolvedModelName", result.complexType);
+        boolean isComplexType = true;
+        if (
+            result.isPrimitiveType ||
+            result.isString ||
+            result.isNumeric ||
+            result.isInteger ||
+            result.isLong ||
+            result.isNumber ||
+            result.isFloat ||
+            result.isDouble ||
+            result.isByteArray ||
+            result.isBinary ||
+            result.isFile ||
+            result.isBoolean ||
+            result.isDate ||
+            result.isDateTime ||
+            result.isUuid ||
+            result.complexType.equals("object")
+        ) {
+            isComplexType = false;
+        }
+        if (isComplexType) {
+            result.vendorExtensions.put("resolvedModelName", result.complexType.substring(0, 1).toUpperCase(Locale.ROOT) + result.complexType.substring(1));
         }
         return result;
     }
@@ -106,7 +127,18 @@ public class StaticMarkdownGenerator extends DefaultCodegen implements CodegenCo
         Map<String, Object> operations = (Map<String, Object>) objs.get("operations");
         List<CodegenOperation> operationList = (List<CodegenOperation>) operations.get("operation");
         for (CodegenOperation op : operationList) {
-            op.httpMethod = op.httpMethod.toLowerCase(Locale.ROOT);
+            op.httpMethod = op.httpMethod.toUpperCase(Locale.ROOT);
+            prepareOperationParameters(op.allParams);
+            prepareOperationParameters(op.bodyParams);
+            prepareOperationParameters(op.pathParams);
+            prepareOperationParameters(op.queryParams);
+            prepareOperationParameters(op.headerParams);
+            prepareOperationParameters(op.formParams);
+            prepareOperationParameters(op.cookieParams);
+            prepareOperationParameters(op.requiredParams);
+            prepareOperationParameters(op.optionalParams);
+            prepareOperationParameters(op.cookieParams);
+            prepareOperationResponses(op.responses);
             for (CodegenResponse response : op.responses) {
                 if ("0".equals(response.code)) {
                     response.code = "default";
@@ -116,4 +148,19 @@ public class StaticMarkdownGenerator extends DefaultCodegen implements CodegenCo
         return objs;
     }
 
+    private void prepareOperationParameters(List<CodegenParameter> parameters) {
+        for (CodegenParameter parameter: parameters) {
+            if (parameter.dataType.length() >= 1 && !parameter.isPrimitiveType) {
+                parameter.vendorExtensions.put("resolvedModelName", parameter.dataType.substring(0, 1).toUpperCase(Locale.ROOT) + parameter.dataType.substring(1));
+            }
+        }
+    }
+
+    private void prepareOperationResponses(List<CodegenResponse> responses) {
+        for (CodegenResponse response: responses) {
+            if (response.dataType.length() >= 1 && !response.primitiveType) {
+                response.vendorExtensions.put("resolvedModelName", response.dataType.substring(0, 1).toUpperCase(Locale.ROOT) + response.dataType.substring(1));
+            }
+        }
+    }
 }
